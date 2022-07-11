8ebb8983ee63e6adaf7d0e56dd8bbbbef2bf931c
Nikita Fordui <nikita.forduy@protonmail.com>
2022-07-11 15:43:29 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ setuptools.setup(
     version=r'1.0.2',
     author=r'Bernhard Schäfl',
     author_email=r'schaefl@ml.jku.at',
-    url=r'https://github.com/ml-jku/hopfield-layers',
+    url=r'https://github.com/nikiandr/hopfield-layers',
     description=r'Continuous modern Hopfield layers for Deep Learning architectures',
     long_description=long_description,
     long_description_content_type=r'text/markdown',
     packages=setuptools.find_packages(),
-    python_requires=r'>=3.8.0',
+    python_requires=r'>=3.7.0',
     install_requires=[
         r'torch>=1.5.0',
         r'numpy>=1.20.0'
